3763e266cd921921e981c865f94c43cc724d352b
SHAD-KING404 <146947193+SHAD-KING404@users.noreply.github.com>
2023-10-21 18:34:52 +0600

    Add files via upload

---
+++ IBRAHIM ENC.py
@@ -0,0 +1,76 @@
+#-----------------INFO----------------#
+#CODING U7P4L1N
+#FACEBOOK > ANONYMOUS CYBER 
+#GITHUB > U7P4L-IN
+#Join Telegram Channel For More Cracking Stuffs
+#https://t.me/AN0NYM0U5_CY83R
+## https://t.me/TheU7p4lArmyX
+## TG : https://t.me/addlist/upzIILcBvVs0ZGJl
+#-------------MAIN>MENU-------------#
+
+
+import marshal,os,sys,time
+os.system('espeak -a 300 " REQUEST,   MY ,  FB ID,"')
+os.system("clear")
+
+os.system("xdg-open https://www.facebook.com/ibrahim.tera.real.bap")
+
+logo = ("""\033[1;92m
+
+
+
+
+
+ \033[1;93m╔═════════════════[\033[1;32m ADMIN - INFO \033[1;32m]═════════════╗
+ \033[1;93m║\033[1;96m[✓] develop BY\33[0;m   : \033[1;96m IBRAHIM VAU               \033[1;32m║
+ \033[1;93m║\x1b[38;5;208m[✓] \x1b[38;5;208mFACEBOK      : \x1b[38;5;208m IBRAHIM VAU               \033[1;32m║
+ \033[1;93m║\033[1;35m[✓] GITHUB       :  \033[1;35mAmi-Akhon-dimu.na        \033[1;32m ║
+ \033[1;93m║\033[1;38m[✓] TOOL STATUS  : \033[1;38m RANDOM \033[1;32m                   ║
+ \033[1;93m║\033[1;32m[✓] TEAM         :\033[1;32    EHC CYBER \033[1;32m                ║
+ \033[1;93m║\033[1;31m[✓] TOOL VIRSION :  \033[1;38m0.1                       \033[1;32m║
+ \033[1;93m║᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈─╌❊❊╌──┈⊰᯽⊱\033[1;32m║
+ \033[1;93m║[\033[1;97m•\033[1;91m]\033[1;32m PLZ SAPPORT ME BRO....                   \033[1;32m ║
+ \033[1;93m║[\033[1;97m•\033[1;91m]\033[1;32m REQUEST MY FB ID IBRAHIM VAU....         \033[1;32m ║
+ \033[1;93m╚═════════════════[\033[1;93m IBR4H1M - 404 \033[1;32m]════════════╝
+
+               \x1b[1;97m\x1b[1;44m DEVELOPER: \x1b[0m\x1b[1;97m\x1b[1;41m 𝐈𝐁𝐑𝐀𝐇𝐈𝐌 \x1b[0m
+
+  \x1b[1;91m--●\x1b[1;92m━═━═━═━━═━═━═\x1b[1;91m❴\033[47m\033[1;30mIBRAHIM VAU\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━═━\x1b[1;91m●-- 
+""")
+def main():
+        try:
+        	   
+                print (logo)
+                print ('    \033[1;32m┏━━━\033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m]\033[1;32m EXAMPLE :\033[33;1m/sdcard/file.py')
+                file = input ('    \033[1;32m┗━━━\033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mFILE NAME :\033[33;1m ')
+                fileopen = open(file).read()
+                a = compile(fileopen, 'dg', 'exec')
+                m = marshal.dumps(a)
+                s = repr(m)
+                b = ('##-----------------------ADMIN>INFO---------------------------##\n# ENCRYPTION BY :IBRAHIM VAU\n# VERSION : 1.0.2\n# GITHUB : https://github.com/IBR4HIM-404\n##------------------------MAIN>MENU-------------------------##\nimport marshal\nexec(marshal.loads(' + s +'))')
+                c = file.replace('.py', '.py')
+                d = open(c, 'w')
+                d.write(b)
+                d.close()
+                print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mENCRYPTION SUCCESSFUL >',c)
+                print('    \033[1;32m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+                time.sleep(1)
+                aq = input ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mWANT TO ENCRYPT AGAIN? [Y/N]')
+                if aq =="":
+                        print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mCOMMAND NOT FOUND !')
+                        os.sys.exit()
+                elif aq =="y" or aq =="Y":
+                        main()
+                else:
+                        if aq =="n" or aq =="N":
+                                print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32m➤THANK YOU BRO ;v\n')
+                        else:
+                                print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mCOMMAND NOT FOUND!')
+                                os.sys.exit()
+        except IOError:
+                print ('   \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mFILE NOT FOUND ! ')
+                time.sleep(1)
+                main()
+
+if __name__=='__main__':
+        main()
